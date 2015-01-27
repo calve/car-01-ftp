a1d67fd8dcad0eaafafaf5f23f7c60e64601adc0
calve <calvinh34@gmail.com>
2015-01-27 18:10:01 +0100

    Basic main. Do not compile yet

---
+++ src/ftpd/Serveur.java
@@ -0,0 +1,20 @@
+package ftpd;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class Serveur{
+    public static final int LISTENING_PORT = 1025; /* Ports under 1024 needs roots permissions to be binded */
+
+    public static void main(String[] args) throws IOException{
+        System.out.println("Welcome in this ftpd java");
+        System.out.println("Now listening on port "+LISTENING_PORT);
+        ServerSocket serverSocket = new ServerSocket(LISTENING_PORT);
+        Socket socket = null;
+        while(true){
+            socket = serverSocket.accept(); /* Wait for an inbound connection */
+            (new FtpRequest(socket)).start();
+        }
+    }
+}
